c5e384db8ce0f8c54ac33b6b279843a4e984de4a
WooJJam <jaemin5548@naver.com>
2024-04-19 21:29:08 +0900

    [Section 6] 로그인 처리1 - 로그인 기능

---
+++ login-start/src/main/java/hello/login/web/login/LoginService.java
@@ -0,0 +1,34 @@
+package hello.login.web.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @param loginId
+     * @param password
+     * @return null 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+//        Member member = findMemberOptional.get();
+//        if (member.getPassword().equals(password)) {
+//            return member;
+//        } else {
+//            return null;
+//        }
+        return memberRepository.findByLoginId(loginId)
+                .filter(member -> member.getPassword().equals(password))
+                .orElse(null);
+    }
+}
