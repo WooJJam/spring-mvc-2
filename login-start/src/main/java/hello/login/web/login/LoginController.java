42c253975cdede56ffeeccf9aa04bdf45be9b033
WooJJam <jaemin5548@naver.com>
2024-04-20 17:33:51 +0900

    [Section 6] 로그인 처리1 - 로그인 처리하기(세션 직접 만들기)

--- login-start/src/main/java/hello/login/web/login/LoginController.java
+++ login-start/src/main/java/hello/login/web/login/LoginController.java
@@ -1,5 +1,6 @@
 package hello.login.web.login;
 
+import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
