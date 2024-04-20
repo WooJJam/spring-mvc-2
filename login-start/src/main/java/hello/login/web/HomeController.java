c6b93728337cab30aa6cb4cd0fe26ae5f8f11f6f
WooJJam <jaemin5548@naver.com>
2024-04-20 19:20:51 +0900

    [Section 6] 로그인 처리1 - 로그인 처리하기 (서블릿 HTTP 세션 1)

--- login-start/src/main/java/hello/login/web/HomeController.java
+++ login-start/src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -40,7 +41,7 @@ public class HomeController {
         return "login/loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         Member member = (Member) sessionManager.getSession(request);
@@ -49,6 +50,25 @@ public class HomeController {
             return "home";
         }
         model.addAttribute("member", member);
+        return "login/loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "login/loginHome";
     }
 }
