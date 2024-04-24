9789df12755536da8f07472bd34d770ef7b2f705
WooJJam <jaemin5548@naver.com>
2024-04-25 02:10:24 +0900

    [Section 8] 예외 처리와 오류 페이지 - 서블릿 예외 처리 (인터셉터)

--- mvc2-exception/src/main/java/hello/mvc2exception/WebConfig.java
+++ mvc2-exception/src/main/java/hello/mvc2exception/WebConfig.java
@@ -1,17 +1,29 @@
 package hello.mvc2exception;
 
 import hello.mvc2exception.filter.LogFilter;
+import hello.mvc2exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico",
+                        "/error", "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
