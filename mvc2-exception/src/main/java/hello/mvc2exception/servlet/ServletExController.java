15f558edbae39a63cfd5e6f69e4e90e49d5a734d
WooJJam <jaemin5548@naver.com>
2024-04-25 02:46:01 +0900

    [Section 8] 예외 처리와 오류 페이지 - 서블릿 예외 처리 (오류 페이지1)

--- mvc2-exception/src/main/java/hello/mvc2exception/servlet/ServletExController.java
+++ mvc2-exception/src/main/java/hello/mvc2exception/servlet/ServletExController.java
@@ -1,5 +1,6 @@
 package hello.mvc2exception.servlet;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,11 @@ public class ServletExController {
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
     }
 
     @GetMapping("/error-500")
