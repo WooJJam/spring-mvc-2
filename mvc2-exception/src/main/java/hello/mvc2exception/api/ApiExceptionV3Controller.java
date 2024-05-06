39647508cb71abbbfb4b6cdb9d58129c4bf35b12
WooJJam <jaemin5548@naver.com>
2024-05-06 23:54:19 +0900

    [Section 9] API 예외 처리 - @ControllerAdvice

--- mvc2-exception/src/main/java/hello/mvc2exception/api/ApiExceptionV3Controller.java
+++ mvc2-exception/src/main/java/hello/mvc2exception/api/ApiExceptionV3Controller.java
@@ -1,19 +1,18 @@
 package hello.mvc2exception.api;
 
 import hello.mvc2exception.exception.UserException;
-import hello.mvc2exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api2")
-public class ApiExceptionV2Controller {
+@RequestMapping("/api3")
+public class ApiExceptionV3Controller {
     @Data
     @AllArgsConstructor
     static class MemberDto {
---
+++ mvc2-exception/src/main/java/hello/mvc2exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,44 @@
+package hello.mvc2exception.exhandler.advice;
+
+import hello.mvc2exception.exception.UserException;
+import hello.mvc2exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice("hello.mvc2exception.api")
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("400", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> notFoundExHandler(NoResourceFoundException e) {
+        log.error("[notFoundExHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("NotFound", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
+    }
+}
