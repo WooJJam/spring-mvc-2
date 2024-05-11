918d9b635076e7de9da3b0a3bf06d786cbf71758
WooJJam <jaemin5548@naver.com>
2024-05-11 16:57:52 +0900

    [Section 10] 스프링 타입 컨버터 - 스프링에 Converter 적용하기

---
+++ typeconverter/src/main/java/WooJJam/typeconverter/WebConfig.java
@@ -0,0 +1,21 @@
+package WooJJam.typeconverter;
+
+import WooJJam.typeconverter.converter.IntegerToStringConverter;
+import WooJJam.typeconverter.converter.IpPortToStringConverter;
+import WooJJam.typeconverter.converter.StringToIntegerConverter;
+import WooJJam.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
