7ab12cea950b15b6efbeac7644ca6411414b1490
WooJJam <jaemin5548@naver.com>
2024-05-13 18:32:46 +0900

    [Section 10] 스프링 타입 컨버터 - 포맷터 적용하기

--- typeconverter/src/main/java/WooJJam/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/WooJJam/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import WooJJam.typeconverter.converter.IntegerToStringConverter;
 import WooJJam.typeconverter.converter.IpPortToStringConverter;
 import WooJJam.typeconverter.converter.StringToIntegerConverter;
 import WooJJam.typeconverter.converter.StringToIpPortConverter;
+import WooJJam.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
